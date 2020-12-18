8c9e163b1e0e0942375b245e713c5c33044c9b0c
Minsiderock <45649592+Minsiderock@users.noreply.github.com>
2020-12-19 00:30:53 +0700

    Line Bot with heroku

---
+++ Project/Config.py
@@ -0,0 +1,4 @@
+Channel_secret = '78878639a82c476ce7926db20d8eb6fc'
+Channel_access_token = '61rshx5yQdeA7wa9JXPgWLNx5heFfqUzD20J+ZUUfGGTiBHaQJfkFY6sv2lzmFb1lsm2B9carzKmMXoIifAuC+R+O4aLx+XYCGq2wlPM9btMIegnnhIDtpzEG/uvTz6GE1wqsVWcHAJfZjQRPiuVqwdB04t89/1O/w1cDnyilFU='
+basic_id = '@394qyens'
+
